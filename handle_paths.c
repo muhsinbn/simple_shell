31a02dea65f3727f2f3c50fc9f2f9c2388360ecc
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-01-05 03:23:55 -0800

    handle_paths.c

---
+++ handle_paths.c
@@ -0,0 +1,147 @@
+#include "shell.h"
+
+/**
+ * add_node - Add anew node to the list.
+ * @head: Ptr to the head of the list.
+ * @path: Path to add.
+ * Return: Ptr to the new node.
+ */
+_list_paths *add_node(_list_paths **head, char *path)
+{
+	char *s_cpy;
+	_list_paths *l;
+
+	l = malloc(sizeof(_list_paths));
+	if (l == NULL)
+		return (NULL);
+
+	if (path)
+	{
+		s_cpy = _strdup(path);
+		if (s_cpy == NULL)
+		{
+			free(l);
+			return (NULL);
+		}
+		l->path = s_cpy;
+		l->len = _strlen(s_cpy);
+	}
+	else
+	{
+		l->path = NULL;
+		l->len = 0;
+	}
+	l->next = (*head);
+	*head = l;
+	return (l);
+}
+
+/**
+ * free_list - Frees a list of paths.
+ * @head: Ptr to the head of the list.
+ */
+void free_list(_list_paths *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free_list(head->next);
+	if (head->path)
+		free(head->path);
+	free(head);
+}
+
+/**
+ * set_all_paths_to_list - Set all paths to a linked list.
+ * Return: Paths list.
+ */
+_list_paths *set_all_paths_to_list()
+{
+	char *path_variable, *path_var_cpy, *token;
+	_list_paths *paths_list;
+
+	paths_list = NULL;
+
+	path_variable = _get_env("PATH");
+	if (path_variable == NULL)
+		return (NULL);
+
+	path_var_cpy = _strdup(path_variable);
+	if (path_var_cpy == NULL)
+		return (NULL);
+	token = _strtok(path_var_cpy, ":");
+	while (token != NULL)
+	{
+		add_node(&paths_list, token);
+		token = _strtok(NULL, ":");
+	}
+	free(path_var_cpy);
+	return (paths_list);
+}
+
+/**
+ * set_all_vector_to_list - Set all environment variables to a linked list.
+ * Return: Paths list.
+ */
+_list_paths *set_all_vector_to_list()
+{
+	char *path_var_cpy;
+	_list_paths *paths_list;
+	int x = 0;
+
+	paths_list = NULL;
+
+	if (environ[x])
+		return (NULL);
+	while (environ[x])
+	{
+		path_var_cpy = environ[x];
+		if (path_var_cpy == NULL)
+			return (NULL);
+		add_node(&paths_list, path_var_cpy);
+		x++;
+	}
+	return (paths_list);
+}
+
+/**
+ * check_access - check access to a file in all paths.
+ * @line_av_1: The command to check access for.
+ * @current: Ptr to the current path in the list.
+ * Return: Full path if found, otherwise NULL.
+ */
+char *check_access(char *line_av_1, _list_paths *current)
+{
+	char *full_path;
+	int found = 0, len;
+
+	if (current == NULL)
+		return (NULL);
+	while (current)
+	{
+		len = _strlen(current->path) + _strlen(line_av_1) + 2;
+		if (len > 1024)
+		{
+			write(STDERR_FILENO, "ERROR: Path too long\n", 21);
+			continue;
+		}
+		full_path = (char *)malloc(len * sizeof(char));
+		_str_cpy(full_path, current->path);
+		_str_cat(full_path, "/");
+		_str_cat(full_path, line_av_1);
+		if (access(full_path, X_OK) == 0)
+		{
+			found = 1;
+			break;
+		}
+		else
+			free(full_path);
+		current = current->next;
+	}
+	if (found)
+		return (full_path);
+
+	else
+		return (NULL);
+}
